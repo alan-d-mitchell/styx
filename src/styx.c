4b73c76231767072cbc06de790d95e8d765825e1
Alan Mitchell <mitchellalan1999@icloud.com>
2025-12-01 19:50:17 -0600

    update: styx.c to use getopt for arg parsing

--- src/styx.c
+++ src/styx.c
@@ -2,48 +2,105 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <getopt.h>
 
 #include "ops.h"
 
-void print_usage() 
+void print_styx_usage() 
 {
     char* message = "sytx: a git implementation project\n\n"
                     "usage: styx <command> [option(s)]\n\n"
-                    "commands: \n\tinit \t\tinitalize a styx repository\n\n"
+                    "commands: \n\tmkrepo \t\tinitalize a styx repository\n\n"
                     "options: \n\t[-h][--help] \tprint this help message and exit\n";
 
     printf("%s\n", message);
 }
 
+void parse_mkrepo_args(int argc, char** argv, int opts) 
+{
+    char* path = NULL;
+    char* branch = NULL;
+
+    optind = opts;
+
+    static struct option _mkrepo[] = {
+        {"help", no_argument, 0, 'h'},
+        {"path", required_argument, 0, 'p'},
+        {"branch", required_argument, 0, 'b'},
+        {0, 0, 0, 0}
+    };
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, "hp:b:", _mkrepo, NULL)) != -1) 
+    {
+        switch (opt) {
+            case 'h':
+                print_mkrepo_usage();
+                exit(0);
+            case 'p':
+                path = optarg;
+                break;
+            case 'b':
+                branch = optarg;
+                break;
+            case '?':
+                fprintf(stderr, "error: unknown option\n");
+                fprintf(stderr, "try: styx mkrepo --help for a list of options\n");
+                exit(1);
+            default:
+                exit(1);
+        }
+    }
+
+    mkrepo(path, branch);
+}
+
 int main(int argc, char** argv) 
 {
     if (argc < 2) {
-        print_usage();
+        print_styx_usage();
         exit(1);
     }
 
-    for (int i = 0; i < argc; i++) 
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+        print_styx_usage();
+        exit(0);
+    }
+
+    static struct option styx[] = {
+        {"help", no_argument, 0, 'h'},
+        {0, 0, 0, 0}
+    };
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, "h", styx, NULL)) != -1) 
     {
-        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-            print_usage();
-            exit(0);
-        }    
-        
-        if (strcmp(argv[i], "init") == 0) 
-        {
-            if (strcmp(argv[i + 1], "-h") == 0 || strcmp(argv[i + 1], "--help") == 0) {
-                print_init_usage();
+        switch (opt) {
+            case 'h':
+                print_styx_usage();
                 exit(0);
-            } else if (strcmp(argv[i + 1], "-p") == 0 || strcmp(argv[i + 1], "--path") == 0) {
-                char* path = argv[i + 2];
-                printf("PLACEHOLDER: styx repository initialized at %s\n", path);
-                exit(0);
-            } else {
-                init();
-                exit(0);
-            }
+            case '?':
+                fprintf(stderr, "error: unknown command\n");
+                fprintf(stderr, "try: styx --help for a list of commands\n");
+                break;
+            default:
+                break;
         }
     }
+
+    if (argv[1][0] != '-') {
+        char* command = argv[1];
+        if (strcmp(command, "mkrepo") == 0) {
+            parse_mkrepo_args(argc, argv, 2);
+            return 0;
+        } else {
+            fprintf(stderr, "error: unknown command '%s'\n", command);
+            print_styx_usage();
+
+            return 1;
+        }
+    }
+    print_styx_usage();
 
     return 0;
 }
